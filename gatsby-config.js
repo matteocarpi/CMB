f623e1251e21f22183dc3bc434f9a7379810a226
Matteo Carpi <matteocarpi94@gmail.com>
2020-11-18 20:07:40 +0100

    Fixing dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,53 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable max-len */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Gatsby Default Starter',
+    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    author: '@gatsbyjs',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     {
-      resolve: `gatsby-source-wordpress-experimental`,
+      resolve: 'gatsby-source-wordpress-experimental',
       options: {
         url:
           // allows a fallback url if WPGRAPHQL_URL is not set in the env, this may be a local or remote WP instance.
-          process.env.WPGRAPHQL_URL ||
-          `http://localhost/graphql`,
+          process.env.WPGRAPHQL_URL
+          || 'http://localhost/graphql',
         schema: {
-          //Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
-          typePrefix: `Wp`,
+          // Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
+          typePrefix: 'Wp',
         },
         develop: {
-          //caches media files outside of Gatsby's default cache an thus allows them to persist through a cache reset.
+          // caches media files outside of Gatsby's default cache an thus allows them to persist through a cache reset.
           hardCacheMediaFiles: true,
         },
         type: {
           Post: {
             limit:
-              process.env.NODE_ENV === `development`
+              process.env.NODE_ENV === 'development'
                 ? // Lets just pull 50 posts in development to make it easy on ourselves (aka. faster).
                 50
                 : // and we don't actually need more than 5000 in production for this particular site
@@ -53,9 +55,9 @@ module.exports = {
           },
         },
       },
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
