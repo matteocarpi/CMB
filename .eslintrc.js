84453a12109aef6f76cfa0fddd4e8ba537edfc47
Matteo Carpi <matteocarpi94@gmail.com>
2020-12-03 14:27:40 +0100

    Updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,48 @@
+const off = 0
+const warn = 1
+const error = 2
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
+  },
+  globals: {
+    test: true,
+    expect: true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
   ],
   parserOptions: {
+    ecmaVersion: 12,
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: [
     'react',
+    'react-hooks',
   ],
-  rules: {
+  settings: {
+    // Cf. https://github.com/yannickcr/eslint-plugin-react#configuration
+    react: {
+      version: 'detect',
+    },
   },
-};
+  rules: {
+    semi: [warn, 'never'],
+    'react-hooks/rules-of-hooks': error,
+    'react-hooks/exhaustive-deps': warn,
+    'react/react-in-jsx-scope': off,
+    'react/jsx-filename-extension': [warn, { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-props-no-spreading': off,
+    'react/prop-types': off,
+    'no-use-before-define': off,
+    'consistent-return': off,
+    'no-plusplus': off,
+    'no-unused-vars': warn,
+  },
+}
