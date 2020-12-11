4a3c865ed0c5d6219dad3b10ff090bc434fe8d16
Matteo Carpi <matteocarpi94@gmail.com>
2020-12-11 11:37:54 +0100

    Changed graphql endpoint

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,8 +32,8 @@ module.exports = {
       options: {
         url:
           // allows a fallback url if WPGRAPHQL_URL is not set in the env, this may be a local or remote WP instance.
-          process.env.WPGRAPHQL_URL
-          || 'http://localhost/graphql',
+          process.env.GRAPHQL_ENDPOINT
+          || 'http://51.178.84.250:777/graphql',
         schema: {
           // Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
           typePrefix: 'Wp',
@@ -47,7 +47,7 @@ module.exports = {
             limit:
               process.env.NODE_ENV === 'development'
                 ? // Lets just pull 50 posts in development to make it easy on ourselves (aka. faster).
-                50
+                10
                 : // and we don't actually need more than 5000 in production for this particular site
                 5000,
           },
