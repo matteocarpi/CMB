41a387cfdb4928f62d2e4d197b39470a8fae7a42
Matteo Carpi <matteocarpi94@gmail.com>
2020-12-23 14:40:11 +0100

    Animate mobile menu

--- src/components/MobileHeader/MobileHeader.jsx
+++ src/components/MobileHeader/MobileHeader.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Link, useStaticQuery, graphql } from 'gatsby'
+import { motion, AnimatePresence } from 'framer-motion'
 
 import LogoSVG from '../../assets/logo/logo-full-dark.svg'
 import BurgerSVG from '../../assets/icons/burger.svg'
@@ -13,16 +14,17 @@ const Container = styled.header`
   left: 0;
   right: 0;
   min-height: 100px;
-
   z-index: 10000;
   background-color: white;
-  border-bottom: solid 1px lightgrey;
+  /* border-bottom: solid 1px lightgrey; */
+  background-color: transparent;
 `
 
 const HeaderTop = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  background-color: white;
 `
 
 const Logo = styled(LogoSVG)`
@@ -36,9 +38,10 @@ const Close = styled(CloseSVG)`
   margin: 0 30px;
 `
 
-const NavigationContainer = styled.nav`
+const NavigationContainer = styled(motion.nav)`
   display: flex;
   justify-content: center;
+  background-color: white;
 `
 
 const Navigation = styled.ul``
@@ -55,6 +58,17 @@ const NavItem = styled.li`
     margin-left: -0.5rem;
   }
 `
+
+const navigationVariants = {
+  hidden: {
+    scaleY: 0,
+    originY: 'top',
+  },
+  visible: {
+    scaleY: 1,
+    originY: 'top',
+  },
+}
 
 export default function MobileHeader() {
   const [isOpen, setIsOpen] = useState(false)
@@ -86,23 +100,30 @@ export default function MobileHeader() {
         </button>
       </HeaderTop>
 
-      {isOpen && (
-        <NavigationContainer>
-          <Navigation>
-            {navigation.allWpPage.edges.map(page => {
-              const p = page.node
+      <AnimatePresence>
+        {isOpen && (
+          <NavigationContainer
+            variants={navigationVariants}
+            initial="hidden"
+            animate="visible"
+            exit={{ scaleY: 0 }}
+          >
+            <Navigation>
+              {navigation.allWpPage.edges.map(page => {
+                const p = page.node
 
-              return (
-                <NavItem key={p.id}>
-                  <Link to={`/${p.slug}`} activeClassName="active">
-                    {p.title}
-                  </Link>
-                </NavItem>
-              )
-            })}
-          </Navigation>
-        </NavigationContainer>
-      )}
+                return (
+                  <NavItem key={p.id}>
+                    <Link to={`/${p.slug}`} activeClassName="active">
+                      {isOpen && p.title}
+                    </Link>
+                  </NavItem>
+                )
+              })}
+            </Navigation>
+          </NavigationContainer>
+        )}
+      </AnimatePresence>
     </Container>
   )
 }
