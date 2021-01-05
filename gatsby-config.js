dc6a515d5bc6ac43e367d6955000c67d4aa9698d
Matteo Carpi <matteocarpi94@gmail.com>
2021-01-05 18:08:17 +0100

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,18 +15,18 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    // {
-    //   resolve: 'gatsby-plugin-manifest',
-    //   options: {
-    //     name: 'gatsby-starter-default',
-    //     short_name: 'starter',
-    //     start_url: '/',
-    //     background_color: '#663399',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'cmb-group',
+        short_name: 'cmb',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#040818',
+        display: 'minimal-ui',
+        icon: 'src/assets/logo/favicon.png', // This path is relative to the root of the site.
+      },
+    },
     {
       resolve: 'gatsby-source-wordpress-experimental',
       options: {
