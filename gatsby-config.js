d6b07b19b15717a166c172b57260fd895e9dc447
Matteo Carpi <matteocarpi94@gmail.com>
2021-05-17 11:37:46 +0200

    Install gatsby cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -110,6 +110,7 @@ module.exports = {
         timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
     },
+    'gatsby-plugin-gatsby-cloud',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
