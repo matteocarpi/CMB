e99cec4876ddf4cb7742334c110816d4f6b41270
fffreddie <info@fastbyte.it>
2022-07-29 10:36:58 +0200

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import styled, { ThemeProvider, createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
-/*import { CookiesProvider } from 'react-cookie'*/
 import { defaultTheme } from './Themes/Themes'
 
 import Header from './Header'
 import Footer from './Footer'
-/*import CookiePopUp from './CookiePopUp/CookiePopUp'*/
 
 import 'swiper/swiper.scss'
 import 'swiper/components/navigation/navigation.scss'
