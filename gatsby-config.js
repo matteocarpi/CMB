98cdf4d84f775a5baeac778fc063d7a4cf59a0f8
fffreddie <info@fastbyte.it>
2022-09-14 11:25:34 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,22 +121,10 @@ module.exports = {
         endpoint: process.env.MAILCHIMP_ENDPOINT, // string; add your MC list endpoint here; see instructions below
         timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
-    },
+   },
     'gatsby-plugin-gatsby-cloud',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-},
-{
-    resolve: "gatsby-plugin-cookiebot",
-    options: {
-      cookiebotId: "38db8364-4db7-4614-9bd5-81a612d544a9", // Required. Site's Cookiebot ID.
-      manualMode: true, // Optional. Turns on Cookiebot's manual mode. Defaults to false.
-      blockGtm: false, //  Optional. Skip blocking of GTM. Defaults to true if manualMode is set to true.
-      includeInDevelopment: true, // Optional. Enables plugin in development. Will cause gatsby-plugin-google-tagmanager to thrown an error when pushing to dataLayer. Defaults to false.
-      pluginDebug: true, // Optional. Debug mode for plugin development. Defaults to false.
-    },
-  },
-],
 }
